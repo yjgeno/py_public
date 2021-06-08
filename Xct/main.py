7e0a7db02a7b234e107e3212ceaf6129dfba9fb1
yoo <77600778+yjgeno@users.noreply.github.com>
2021-06-07 20:15:44 -0500

    Update main.py

--- Xct/main.py
+++ Xct/main.py
@@ -8,16 +8,7 @@ sc.settings.verbosity = 0
 # adata object built
 # for Cell A, B
 
-def Xct_DB(specis = 'Human'):
-    if specis == 'mouse':
-        LR = pd.read_csv('https://raw.githubusercontent.com/yjgeno/Ligand-Receptor-Pairs/master/Mouse/Mouse-2020-Jin-LR-pairs.csv')
-    else:
-        LR = pd.read_csv('https://raw.githubusercontent.com/yjgeno/Ligand-Receptor-Pairs/master/Human/Human-2020-Jin-LR-pairs.csv')
-    receptors = LR['receptor'].str.split('_', expand=True)
-    receptors.columns = ['rec_A', 'rec_B', 'rec_C']
-    LRs = pd.concat([LR[['pathway_name', 'ligand']], receptors], axis=1)
-    del LR
-    return LRs
+
 
 
 def XctInfo(ada, CellA, CellB, permute = False, verbose = False):
