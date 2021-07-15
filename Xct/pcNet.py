5a82faca7412838ac7fc82cca3f1b12d500214ea
yoo <77600778+yjgeno@users.noreply.github.com>
2021-07-15 15:35:51 -0500

    Create pcNet.py

---
+++ Xct/pcNet.py
@@ -0,0 +1,57 @@
+import numpy as np
+from numpy.linalg import svd
+
+
+def pcNet(X, nComp = 3, scale = True, symmetric = False, q = 0): # X: cell * gene, q: 0-100
+    if not isinstance(X, np.ndarray):
+        raise ValueError('Input should be a numpy array with cells as columns and genes as rows')
+    elif nComp < 2 or nComp >= X.shape[1]:
+        raise ValueError('nComp should be greater or equal than 2 and lower than the total number of genes')
+    
+    else:
+        n = X.shape[1] # genes
+        def pcCoefficients(K):
+            if X is None:
+                raise ValueError('Input should be a matrix with cells as columns and genes as rows')
+            elif nComp < 2 or nComp >= n:
+                raise ValueError('nComp should be greater or equal than 2 and lower than the total number of genes')
+            else:
+                y = X[:, K] 
+                Xi = np.delete(X, K, 1)
+                U, s, VT = svd(Xi, full_matrices=False) 
+                #print ('U:', U.shape, 's:', s.shape, 'VT:', VT.shape)
+                V = VT[:nComp, :].T
+                #print('V:', V.shape)
+
+                score = Xi@V
+                t = np.sqrt(np.sum(score**2, axis=0))
+                score_lsq = ((score.T / (t**2)[:, None])).T
+                beta = np.sum(y[:, None]*score_lsq, axis=0)
+                beta = V@beta
+
+            return list(beta)
+        
+        B = []
+        for k in range(n):
+            B.append(pcCoefficients(k))
+        B = np.array(B)
+        
+        A = np.ones((n, n), dtype=float)
+        np.fill_diagonal(A, 0)
+        for i in range(n):
+            A[i, A[i, :]==1] = B[i, :]
+                 
+        if scale:
+            absA = abs(A)
+            A = A / np.max(absA)
+        if q > 0:
+            A[absA < np.percentile(absA, q)] = 0
+        if symmetric: # place in the end
+            A = (A + A.T)/2
+        #diag(A) <- 0
+        
+        return A
+      
+      
+      
+      
